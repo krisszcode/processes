5558a05ed04cf3afd5a044042996300d7ea839d9
viktorferenczi <ferencziviktor11@gmail.com>
2020-01-20 14:49:32 +0100

    adding

--- processes/Program.cs
+++ processes/Program.cs
@@ -1,13 +1,1 @@
-//almassdadad
-using System;
-
-namespace processes
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿
