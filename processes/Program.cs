d59e4b6f17d46549cc647620a59583ff6e6618d9
krisszcode <kicsikrissz111@gmail.com>
2020-01-20 09:47:51 +0100

    alma

---
+++ processes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace processes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
