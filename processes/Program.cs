c0568d00a001073429745b9e4c303afc6fbfb0db
krisszcode <kicsikrissz111@gmail.com>
2020-01-20 17:34:30 +0100

    alma

---
+++ processes/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Diagnostics;
+
+
+namespace processes
+{
+    class Program
+    {
+        static void Main()
+        {
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
+                if (theprocess.ProcessName.Length > 0)
+                {
+                    try
+                    {
+                        DateTime now = DateTime.Now;
+                        
+                        DateTime StartTime = theprocess.StartTime;
+                        DateTime EndTime = now;
+                        long diffTicks = (EndTime - StartTime).Ticks;
+
+                        TimeSpan t = TimeSpan.FromSeconds(diffTicks);
+
+                        string answer = string.Format("{0:D2}h:{1:D2}m:{2:D2}s:{3:D3}ms",
+                                        t.Hours,
+                                        t.Minutes,
+                                        t.Seconds,
+                                        t.Milliseconds);
+                        Console.WriteLine(answer);
+                        Console.WriteLine("Process Name: {0} ID: {1} Memory: {2} MB  Start time: {3} Running time: {4}",
+                            theprocess.ProcessName, theprocess.Id, (theprocess.WorkingSet64 / 1024f / 1024f), theprocess.StartTime, answer);
+                    }
+                    catch (Exception)
+                    {
+
+                        
+                    }
+                    
+
+
+
+                }
+
+            }
+
+
+        }
+
+
+    }
+}
---
+++ processes/process.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace processes
+{
+    public class Proces
+    {
+        public string ProcessName { get; set; }
+        public int ProcessId { get; set; }
+        public double MemoryUsage { get; set; }
+        public DateTime RunningTime { get; set; }
+        public DateTime StartTime { get; set; }
+
+        public Proces(string processName, int processId, double memoryUsage, DateTime runningTime, DateTime startTime)
+        {
+            ProcessName = processName;
+            ProcessId = processId;
+            MemoryUsage = memoryUsage;
+            RunningTime = runningTime;
+            StartTime = startTime;
+        }
+    }
+}
